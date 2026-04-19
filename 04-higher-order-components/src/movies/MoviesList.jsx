fc898d5abc44c6278d365450cba930917155692f
BharathBanti <dasarib180@gmail.com>
2026-04-19 14:35:43 +0530

    completed

--- 04-higher-order-components/src/movies/MoviesList.jsx
+++ 04-higher-order-components/src/movies/MoviesList.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 function MoviesList({ data }) {
   return (
     <div>
+      <h2>📋Movies List</h2>
       <ul>
         {data.map((movie) => (
           <li key={movie.id}>
-            <p>{movie.title}</p>
-            <p>{movie.director}</p>
-            <p>{movie.year}</p>
+            <p>Title: {movie.title}</p>
+            <p>Director: {movie.director}</p>
+            <p>Released on: {movie.year}</p>
           </li>
         ))}
       </ul>
